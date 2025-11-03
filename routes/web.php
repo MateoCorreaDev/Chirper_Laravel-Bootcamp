73f8c6a7143301e557f646bf122ac15ba60904de
MateoCorreaDev <mateo.dev738@gmail.com>
2025-11-03 14:30:06 -0500

    Register user, routing and views update

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,24 @@
 <?php
 
+use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
 use illuminate\Support\Facades\Route;
 
+//Chirps routes
 Route::get('/', [ChirpController::class, 'index']);
-Route::post('/chirps' , [ ChirpController::class, 'store']);
+Route::post('/chirps', [ChirpController::class, 'store']);
 Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
 Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
 Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
 
 // Route::resource('/chirps', ChirpController::class)
 //     ->only(['store', 'edit' , 'update', 'destroy' ]);
+
+
+//Register routes
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', Register::class)
+    ->middleware('guest');
